ba258d72b6a88b8bdfe519766cffde233fc79b7a
Alexis Grangier <grangierale@gmail.com>
2026-06-05 14:56:01 +0200

    test with whole Britanny

--- test_script.py
+++ test_script.py
@@ -5,7 +5,7 @@ import folium
 # import the data for high tension power lines in Finistère with osmnx. 
 # the data comes from openstreetmap APIs, and is stored in a GeoDataFrame named 'power'. Each row is a power line segment.
 # It is open source and contribution-based hence the sparse network.
-power = ox.features_from_place("Morbihan, France", tags={"power": "line"})
+power = ox.features_from_place("Quimper, France", tags={"power": "line"})
 
 #create the openstreetmap leaflet centered on [LOCATION]. the object is an html map
 m = folium.Map(location=[48.0, -2.9], #currently somewhere around Pontivy
